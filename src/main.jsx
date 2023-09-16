deb68d21fbf2f23fd21e851222023e1f9398afa4
Enzo Farias <enzofarias030603@gmail.com>
2023-09-16 14:11:43 -0300

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,7 @@ import { App } from './App'
 import './index.css'
 
 const pexel = (id) => `https://images.pexels.com/photos/${id}/pexels-photo-${id}.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260`;
-let random = Math.floor(Math.random() * 1000000);
-console.log(random);
+
 
 const images = [
   // Front
